227308c7c0a5501eca535de41de4cff630520c03
Alexander Graus <alegraus31@gmail.com>
2020-08-28 11:58:41 -0300

    crecion de clase AeroLineaModel

---
+++ src/modelo/aerolinea/AeroLineaModel.java
@@ -0,0 +1,51 @@
+package modelo.aerolinea;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static modelo.ConexionBD.getConnection;
+
+public class AeroLineaModel {
+    
+    private static PreparedStatement ps = null;
+    private static ResultSet rs = null;
+    
+    public static AeroLinea[] getAeroLineas(){
+        AeroLinea lineas[]=null;
+        Connection conexion = null;
+        
+        try {
+            conexion = getConnection();
+            ps = conexion.prepareStatement("select count(*) as 'filas' from empresa");
+            rs = ps.executeQuery();
+            if(rs.next()){
+                lineas = new AeroLinea[rs.getInt("filas")];
+            }
+            rs.close();
+            
+            ps = conexion.prepareStatement("select nombre from empresa");
+            rs = ps.executeQuery();
+            
+            int i = 0;
+            while (rs.next()) {
+               lineas[i] = new AeroLinea(rs.getString("nombre"));
+               i++;
+            }
+            rs.close();
+        }catch(SQLException ex){
+            System.err.println("SQl Error: "+ex);
+        } finally {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(AeroLineaModel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        
+        return lineas;
+    }
+}
