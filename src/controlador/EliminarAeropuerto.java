da7978c05c8ca0fef4dc8d294392740a5be8f080
Alexander Graus <alegraus31@gmail.com>
2020-08-30 12:49:51 -0300

    creacion de controller EliminarAeropuerto

---
+++ src/controlador/EliminarAeropuerto.java
@@ -0,0 +1,22 @@
+
+package controlador;
+
+import static gestor_aeropuertos.Gestor_Aeropuertos.controlInicio;
+import javax.swing.JOptionPane;
+import modelo.aeropuerto.AeropuertoModel;
+
+public class EliminarAeropuerto {
+
+    public EliminarAeropuerto(String nombre) {
+        int res = JOptionPane.showConfirmDialog (null, "Seguro que desea eliminar el aeropuerto "+nombre+"?",
+                "Atencion!",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+        
+        if(res == 0){
+            JOptionPane.showMessageDialog(null, (AeropuertoModel.deleteByNombre(nombre)) ? 
+                    "Aeropuerto eliminado con exito!": "Error al eliminar aeropuerto");
+            //recargo la lista de aeropuertos
+            controlInicio.cargarAeropuertos();
+        }
+    }
+    
+}
