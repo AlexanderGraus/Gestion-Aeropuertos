31ad943b140dc650622eecfaa8307ef945f58cc6
Alexander Graus <alegraus31@gmail.com>
2020-08-29 21:24:54 -0300

    creacion de la vista VerAeropuerto

--- src/vista/VerAeropuerto.java
+++ src/vista/VerAeropuerto.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vista;
 
-/**
- *
- * @author ale61
- */
-public class AgregarAeropuerto extends javax.swing.JDialog {
+public class VerAeropuerto extends javax.swing.JDialog {
 
     /**
      * Creates new form AgregarAeropuerto
      */
-    public AgregarAeropuerto(java.awt.Frame parent, boolean modal) {
+    public VerAeropuerto(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         this.setLocationRelativeTo(null);
@@ -36,8 +28,7 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
         cajaNombre = new javax.swing.JTextField();
         cajaCiudad = new javax.swing.JTextField();
         cajaPais = new javax.swing.JTextField();
-        bAgregar = new javax.swing.JButton();
-        bCancelar = new javax.swing.JButton();
+        bCerrar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         lista = new javax.swing.JList<>();
         jLabel5 = new javax.swing.JLabel();
@@ -45,7 +36,7 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
-        jLabel1.setText("Agregar Aeropuerto");
+        jLabel1.setText("Datos del Aeropuerto");
 
         jLabel2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         jLabel2.setText("Nombre:");
@@ -62,12 +53,10 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
 
         cajaPais.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
 
-        bAgregar.setText("Agregar");
-
-        bCancelar.setText("Cancelar");
-        bCancelar.addActionListener(new java.awt.event.ActionListener() {
+        bCerrar.setText("Cerrar");
+        bCerrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bCancelarActionPerformed(evt);
+                bCerrarActionPerformed(evt);
             }
         });
 
@@ -75,18 +64,12 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
         jScrollPane1.setViewportView(lista);
 
         jLabel5.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jLabel5.setText("<html><body>Seleccionar aerolineas<br>del aeropuerto:</body></html>");
+        jLabel5.setText("<html><body>Aerolineas en el<br> aeropuerto:</body></html>");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(52, 52, 52)
-                .addComponent(bAgregar)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 98, Short.MAX_VALUE)
-                .addComponent(bCancelar)
-                .addGap(88, 88, 88))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -113,8 +96,11 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(86, 86, 86)
-                        .addComponent(jLabel1)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(148, 148, 148)
+                        .addComponent(bCerrar)))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -137,19 +123,17 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(bAgregar)
-                    .addComponent(bCancelar))
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addComponent(bCerrar)
+                .addGap(21, 21, 21))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void bCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bCancelarActionPerformed
+    private void bCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bCerrarActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_bCancelarActionPerformed
+    }//GEN-LAST:event_bCerrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -168,21 +152,23 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AgregarAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VerAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AgregarAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VerAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AgregarAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VerAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AgregarAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VerAeropuerto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                AgregarAeropuerto dialog = new AgregarAeropuerto(new javax.swing.JFrame(), true);
+                VerAeropuerto dialog = new VerAeropuerto(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
@@ -195,8 +181,7 @@ public class AgregarAeropuerto extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public javax.swing.JButton bAgregar;
-    public javax.swing.JButton bCancelar;
+    public javax.swing.JButton bCerrar;
     public javax.swing.JTextField cajaCiudad;
     public javax.swing.JTextField cajaNombre;
     public javax.swing.JTextField cajaPais;
