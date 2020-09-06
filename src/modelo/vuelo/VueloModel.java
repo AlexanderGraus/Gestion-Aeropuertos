a249dc758c99c46aa82aeffccf364150d53e71b0
Alexander Graus <alegraus31@gmail.com>
2020-09-06 18:30:14 -0300

    ceracion de funcion getVuelosByIdEmpresa

--- src/modelo/vuelo/VueloModel.java
+++ src/modelo/vuelo/VueloModel.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo.vuelo;
 
-/**
- *
- * @author ale61
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static modelo.ConexionBD.getConnection;
+
 public class VueloModel {
-    
+
+    private static PreparedStatement ps = null;
+    private static ResultSet rs = null;
+
+    public static Vuelo[] getVuelosByIdEmpresa(int idEmpresa) {
+        Vuelo vuelos[] = null;
+        Connection conexion = null;
+
+        try {
+            conexion = getConnection();
+            ps = conexion.prepareStatement("SELECT count(id) as 'filas' FROM vuelo WHERE idEmpresa = ?");
+            ps.setInt(1, idEmpresa);
+
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                vuelos = new Vuelo[rs.getInt("filas")];
+
+                ps = conexion.prepareStatement("SELECT id, nombre, origen,destino,precio"
+                        + " FROM vuelo WHERE idEmpresa = ?");
+                ps.setInt(1, idEmpresa);
+                rs = ps.executeQuery();
+                
+                int i=0;
+                while(rs.next()){
+                    vuelos[i]= new Vuelo(rs.getInt("id"),rs.getString("nombre"),
+                            rs.getString("origen"),rs.getString("destino"),
+                            rs.getFloat("precio"), rs.getInt("idEmpresa"));
+                    
+                    i++;
+                }
+            }
+
+        } catch (SQLException ex) {
+            System.err.println("SQL Error: " + ex);
+        }finally {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                System.err.println("SQL Error: " + ex);
+            }
+        }
+        return vuelos;
+    }
 }
