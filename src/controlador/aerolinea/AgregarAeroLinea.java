2ebe0d273f21b8df6f0ba629d98c3e410761e978
Alexander Graus <alegraus31@gmail.com>
2020-09-06 19:10:47 -0300

    bCancelar cierra la vista AgregarAeroLinea

--- src/controlador/aerolinea/AgregarAeroLinea.java
+++ src/controlador/aerolinea/AgregarAeroLinea.java
@@ -1,5 +1,6 @@
 package controlador.aerolinea;
 
+import java.awt.event.ActionEvent;
 import vista.aeropuerto.Inicio;
 public class AgregarAeroLinea {
     private vista.aerolinea.AgregarAeroLinea vista;
@@ -7,7 +8,13 @@ public class AgregarAeroLinea {
     public AgregarAeroLinea(Inicio vistaInicio){
         this.vista = new vista.aerolinea.AgregarAeroLinea(vistaInicio,true);
         vista.setLocationRelativeTo(null);
-
+        oyentes();
         vista.setVisible(true);
+    }
+
+    private void oyentes() {
+        vista.bCancelar.addActionListener((ActionEvent ae) -> {
+            vista.dispose();
+        });
     }
 }
