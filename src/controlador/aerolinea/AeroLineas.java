2247681fec83fe47f8e631824379e3b8014fdd06
Alexander Graus <alegraus31@gmail.com>
2020-09-06 17:40:51 -0300

    funcion cargar AeroLineas

--- src/controlador/aerolinea/AeroLineas.java
+++ src/controlador/aerolinea/AeroLineas.java
@@ -1,5 +1,8 @@
 package controlador.aerolinea;
 
+import javax.swing.DefaultListModel;
+import modelo.aerolinea.AeroLinea;
+import modelo.aerolinea.AeroLineaModel;
 import vista.aeropuerto.Inicio;
 public class AeroLineas {
     private vista.aerolinea.AeroLineas vista;
@@ -8,6 +11,18 @@ public class AeroLineas {
         //creo la vista
         vista = new vista.aerolinea.AeroLineas(vistaInicio,true);
         vista.setLocationRelativeTo(null);
+        cargarAerolineas();
         vista.setVisible(true);
+    }
+    
+    private void cargarAerolineas(){
+        AeroLinea lineas[] = AeroLineaModel.getAeroLineas();
+        
+        DefaultListModel modelo = new DefaultListModel();
+        for(AeroLinea a: lineas){
+            modelo.addElement(a);
+        }
+        
+        vista.lista.setModel(modelo);
     }
 }
