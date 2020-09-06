b688e6e30a1ff544fc48ebdaeb870dd10e6ed973
Alexander Graus <alegraus31@gmail.com>
2020-09-06 19:04:01 -0300

    controlador AgregarAerolinea crea una nueva vista

--- src/controlador/aerolinea/AgregarAeroLinea.java
+++ src/controlador/aerolinea/AgregarAeroLinea.java
@@ -1,6 +1,13 @@
 package controlador.aerolinea;
 
-
+import vista.aeropuerto.Inicio;
 public class AgregarAeroLinea {
+    private vista.aerolinea.AgregarAeroLinea vista;
     
+    public AgregarAeroLinea(Inicio vistaInicio){
+        this.vista = new vista.aerolinea.AgregarAeroLinea(vistaInicio,true);
+        vista.setLocationRelativeTo(null);
+
+        vista.setVisible(true);
+    }
 }
