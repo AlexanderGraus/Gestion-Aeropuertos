6e98922caf49b66c270ff3c08b9ddba0adbdfc1b
Alexander Graus <alegraus31@gmail.com>
2020-09-06 19:09:08 -0300

    bAgregar crea un controlador AgregarAerolinea

--- src/controlador/aerolinea/AeroLineas.java
+++ src/controlador/aerolinea/AeroLineas.java
@@ -1,17 +1,20 @@
 package controlador.aerolinea;
 
+import java.awt.event.ActionEvent;
 import javax.swing.DefaultListModel;
 import modelo.aerolinea.AeroLinea;
 import modelo.aerolinea.AeroLineaModel;
 import vista.aeropuerto.Inicio;
 public class AeroLineas {
     private vista.aerolinea.AeroLineas vista;
-    
+    private Inicio vistaInicio;
     public AeroLineas(Inicio vistaInicio){
         //creo la vista
+        this.vistaInicio = vistaInicio;
         vista = new vista.aerolinea.AeroLineas(vistaInicio,true);
         vista.setLocationRelativeTo(null);
         cargarAerolineas();
+        oyentes();
         vista.setVisible(true);
     }
     
@@ -24,5 +27,11 @@ public class AeroLineas {
         }
         
         vista.lista.setModel(modelo);
+    }
+
+    private void oyentes() {
+        vista.bAgregar.addActionListener((ActionEvent ae) -> {
+            new AgregarAeroLinea(vistaInicio);
+        });
     }
 }
