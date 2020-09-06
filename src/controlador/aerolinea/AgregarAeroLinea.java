5d4cfc083de77409ec0c16423426024c6a2ccd4e
Alexander Graus <alegraus31@gmail.com>
2020-09-06 19:46:56 -0300

    insercion de una nueva aerolinea en actionEvent del boton agregar

--- src/controlador/aerolinea/AgregarAeroLinea.java
+++ src/controlador/aerolinea/AgregarAeroLinea.java
@@ -1,7 +1,11 @@
 package controlador.aerolinea;
 
 import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import modelo.aerolinea.AeroLinea;
+import modelo.aerolinea.AeroLineaModel;
 import vista.aeropuerto.Inicio;
+import controlador.aeropuerto.InicioController;
 public class AgregarAeroLinea {
     private vista.aerolinea.AgregarAeroLinea vista;
     
@@ -15,6 +19,19 @@ public class AgregarAeroLinea {
     private void oyentes() {
         vista.bCancelar.addActionListener((ActionEvent ae) -> {
             vista.dispose();
+        });
+        
+        vista.bAgregar.addActionListener((ActionEvent ae) ->{
+            AeroLinea linea = new AeroLinea();
+            linea.setNombre(vista.cajaNombre.getText());
+            
+            //inserto la nueva aerolinea en la BD
+            JOptionPane.showMessageDialog(null,(AeroLineaModel.create(linea)) ? 
+                    "Aerolinea ingresada con exito": "Error al ingresar la aerolinea");
+            
+            vista.dispose();
+            
+
         });
     }
 }
