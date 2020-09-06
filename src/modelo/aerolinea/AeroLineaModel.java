1352cb23cf8929852f562ad7600f036bf184df95
Alexander Graus <alegraus31@gmail.com>
2020-09-06 19:19:33 -0300

    funcion create dentro de AeroLineaModel

--- src/modelo/aerolinea/AeroLineaModel.java
+++ src/modelo/aerolinea/AeroLineaModel.java
@@ -4,39 +4,39 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static modelo.ConexionBD.getConnection;
 
 public class AeroLineaModel {
-    
+
     private static PreparedStatement ps = null;
     private static ResultSet rs = null;
-    
-    public static AeroLinea[] getAeroLineas(){
-        AeroLinea lineas[]=null;
+
+    public static AeroLinea[] getAeroLineas() {
+        AeroLinea lineas[] = null;
         Connection conexion = null;
-        
+
         try {
             conexion = getConnection();
             ps = conexion.prepareStatement("select count(*) as 'filas' from empresa");
             rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 lineas = new AeroLinea[rs.getInt("filas")];
             }
 
-            
             ps = conexion.prepareStatement("select id,nombre from empresa");
             rs = ps.executeQuery();
-            
+
             int i = 0;
             while (rs.next()) {
-               lineas[i] = new AeroLinea(rs.getInt("id"),rs.getString("nombre"));
-               i++;
+                lineas[i] = new AeroLinea(rs.getInt("id"), rs.getString("nombre"));
+                i++;
             }
             rs.close();
-        }catch(SQLException ex){
-            System.err.println("SQl Error: "+ex);
+        } catch (SQLException ex) {
+            System.err.println("SQl Error: " + ex);
         } finally {
             try {
                 conexion.close();
@@ -45,7 +45,37 @@ public class AeroLineaModel {
             }
 
         }
-        
+
         return lineas;
+    }
+
+    public static boolean create(AeroLinea aerolinea) {
+        Connection conexion = null;
+        boolean estado = true;
+        int res = 0;
+        try {
+            conexion = getConnection();
+            ps = conexion.prepareStatement("INSERT into empresa (nombre) values (?)");
+
+            ps.setString(1, aerolinea.getNombre());
+
+            res = ps.executeUpdate();
+
+            estado = res > 0;
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            System.err.println("SQL Error: " + ex);
+            estado = false;
+
+        } finally {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                System.err.println("SQL Error: " + ex);
+            }
+        }
+        return estado;
     }
 }
