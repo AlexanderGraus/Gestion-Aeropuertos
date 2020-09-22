cd1c22c38e8638e61959084fc6d148bcc5984744
Alexander Graus <alegraus31@gmail.com>
2020-09-22 15:18:02 -0300

    actualiza la aerolinea desde el controlador EditarAerolinea

--- src/controlador/aerolinea/EditarAeroLinea.java
+++ src/controlador/aerolinea/EditarAeroLinea.java
@@ -1,91 +1,46 @@
-package controlador.aeropuerto;
+package controlador.aerolinea;
 
-import static gestor_aeropuertos.Gestor_Aeropuertos.controlInicio;
 import java.awt.event.ActionEvent;
-import javax.swing.DefaultListModel;
 import javax.swing.JOptionPane;
 import modelo.aerolinea.AeroLinea;
 import modelo.aerolinea.AeroLineaModel;
-import modelo.aeropuerto.Aeropuerto;
-import modelo.aeropuerto.AeropuertoModel;
 import vista.aeropuerto.Inicio;
-
-public class EditarAeropuerto {
-
-    private vista.aeropuerto.AgregarAeropuerto vista;
-    private String nombre;
-
-    public EditarAeropuerto(Inicio vistaInicio, String nombre) {
-        //voy a usar la misma vista que AgregarAeropuerto pero le cambio los titulos
-        this.vista = new vista.aeropuerto.AgregarAeropuerto(vistaInicio, true);
-        this.vista.etiqTitulo.setText("Editar Aeropuerto");
-        this.vista.bAgregar.setText("Editar");
-        
-        this.nombre = nombre;
+public class EditarAeroLinea {
+    private vista.aerolinea.AgregarAeroLinea vista;
+    private AeroLinea linea;
+    public EditarAeroLinea(Inicio vistaInicio,AeroLinea linea){
+        this.vista = new vista.aerolinea.AgregarAeroLinea(vistaInicio,true);
+        this.linea = linea;
+        //voy a usar la misma vista que AgregarAeroLinea pero le cambio los titulos
+        vista.etiqTitulo.setText("Editar Aerolinea");
+        vista.bAgregar.setText("Editar");
+        vista.setLocationRelativeTo(null);
         oyentes();
-        cargarAeropuerto();
-        cargarAeroLineas();
-        
-        
-        this.vista.setVisible(true);
-
+        cargarAeroLinea();
+        vista.setVisible(true);
     }
 
-    private void cargarAeropuerto() {
-        Aeropuerto aeropuerto = AeropuertoModel.getAeropuertoByNombre(nombre);
-
-        this.vista.cajaNombre.setText(nombre);
-        this.vista.cajaCiudad.setText(aeropuerto.getCiudad());
-        this.vista.cajaPais.setText(aeropuerto.getPais());
-
-        //cargo las aerolineas en un model
-        DefaultListModel modelo = new DefaultListModel();
-        for (AeroLinea a : aeropuerto.getLineas()) {
-            modelo.addElement(a);
-        }
-
-        this.vista.lista.setModel(modelo);
-    }
-    
-    private void cargarAeroLineas() {
-        AeroLinea lineas[];
-
-        lineas = AeroLineaModel.getAeroLineas();
-
-        DefaultListModel modeloLista = new DefaultListModel();
-        for (AeroLinea linea : lineas) {
-            modeloLista.addElement(linea);
-        }
-        
-        
-
-        vista.lista.setModel(modeloLista);
-    }
-    
     private void oyentes() {
-        this.vista.bCancelar.addActionListener((ActionEvent ae) ->{
-            this.vista.dispose();
-        });
-        
-        this.vista.bAgregar.addActionListener((ActionEvent ae) ->{
-            Aeropuerto aeropuerto = new Aeropuerto();
-            
-            //cargo los datos del nuevo aeropuerto segun los datos de la vista
-            aeropuerto.setNombre(vista.cajaNombre.getText());
-            aeropuerto.setCiudad(vista.cajaCiudad.getText());
-            aeropuerto.setPais(vista.cajaPais.getText());
-            AeroLinea lineas[] = new AeroLinea[vista.lista.getSelectedValuesList().size()];
-            lineas = vista.lista.getSelectedValuesList().toArray(lineas);
-            aeropuerto.setLineas(lineas);
-            
-            //el id no lo puedo sacar de la vista sino de la BD
-            aeropuerto.setId(AeropuertoModel.getIdAeropuertoByNombre(nombre));
-
-            JOptionPane.showMessageDialog(null, (AeropuertoModel.update(aeropuerto)) ? 
-                    "Aeropuerto editado con exito":"Error al editar el aeropuerto");
-            controlInicio.cargarAeropuertos();
+        vista.bCancelar.addActionListener((ActionEvent ae) -> {
             vista.dispose();
         });
+        
+        vista.bAgregar.addActionListener((ActionEvent ae) ->{
+            linea.setNombre(vista.cajaNombre.getText());
+            
+            //modifico la nueva aerolinea en la BD
+            JOptionPane.showMessageDialog(null,(AeroLineaModel.update(linea)) ? 
+                    "Aerolinea editada con exito": "Error al editar la aerolinea");
+            
+            vista.dispose();
+            
+
+        });
+    }
+
+    private void cargarAeroLinea() {
+        vista.cajaNombre.setText(linea.getNombre());
+        
     }
 }
 
